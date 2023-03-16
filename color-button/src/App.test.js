a24839b70552b122733f8addeecf0b25a932baf3
veronika-simic <veronikasimic21@gmail.com>
2023-03-16 07:03:31 +0100

    failing tests for button text and color

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('button has correct initial color', () => {
+  render(<App/>)
+  const button = screen.getByRole('button', {name: 'Change to blue'})
+  expect(button).toHaveStyle({backgroundColor: 'red'})
 });
+
+// no need for this test since its in the test above
+test('button has correct initial text', () => {
+  render(<App/>)
+})
+
+test('button turns blue when clicked', () => {
+
+})
