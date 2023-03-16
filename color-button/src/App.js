dbc08c9f786fff0fe2350202427cdcba3618468b
veronika-simic <veronikasimic21@gmail.com>
2023-03-16 07:48:23 +0100

    test passing for checkbox and button

--- color-button/src/App.js
+++ color-button/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import { useState } from 'react';
 function App() {
   const [color, setColor] = useState('red');
+  const [disabled, setDisabled] = useState(false)
 
   const newColor = color === 'red' ? 'blue' : 'red'
   return (
     <div>
-      <button style={{backgroundColor: color}} onClick={() => setColor(newColor) }>Change to {newColor}</button>
-      <input type='checkbox' ></input>
+      <button style={{backgroundColor: color}} onClick={() => setColor(newColor) } disabled={disabled}>Change to {newColor}</button>
+      <input type='checkbox' onChange={(e) => setDisabled(e.target.checked)}></input>
     </div>
   );
 }
