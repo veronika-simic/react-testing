37df0ff9838ba6a81a102ac4644a71b62d1e04e9
veronika-simic <veronikasimic21@gmail.com>
2023-03-16 07:16:07 +0100

    failing tests for color and text change

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial color', () => {
   render(<App/>)
+
   const button = screen.getByRole('button', {name: 'Change to blue'})
   expect(button).toHaveStyle({backgroundColor: 'red'})
 });
@@ -12,6 +13,10 @@ test('button has correct initial text', () => {
   render(<App/>)
 })
 
-test('button turns blue when clicked', () => {
-
+test('button turns blue when clicked and changes text', () => {
+  render(<App/>)
+  const button = screen.getByRole('button', {name: 'Change to blue'})
+  fireEvent.click(button);
+  expect(button).toHaveStyle({backgroundColor: 'blue'})
+  expect(button).toHaveTextContent('Change to red')
 })
