aedb15a2342a448cab749b39adc5319437a4f369
veronika-simic <veronikasimic21@gmail.com>
2023-03-16 07:36:04 +0100

    failing test for checkbox

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -20,3 +20,17 @@ test('button turns blue when clicked and changes text', () => {
   expect(button).toHaveStyle({backgroundColor: 'blue'})
   expect(button).toHaveTextContent('Change to red')
 })
+
+
+test('initial conditions', () => {
+  render(<App/>)
+  const button = screen.getByRole('button', {name: 'Change to blue'})
+  // check that button is enabled if the checkbox is not clicked 
+  expect(button).toBeEnabled()
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
+   // if checkbox is clicked button is disabled
+ /*  fireEvent.click(checkbox);
+  expect(button).toBeDisabled() */
+ 
+})
